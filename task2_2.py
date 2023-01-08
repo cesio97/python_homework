ac621708a02430cbf5abcab6a3c3183587b6cddd
cesio97 <114251017+cesio97@users.noreply.github.com>
2023-01-08 18:47:43 +0300

    Add files via upload

---
+++ task2_2.py
@@ -0,0 +1,7 @@
+# Выведите таблицу истинности для выражения ¬(X ∧ Y) ∨ Z.
+
+print(f'x | y | z | ¬(x ∧ y) ∨ z')
+for x in range (2):
+    for y in range (2):
+        for z in range (2):
+            print(f'{x} | {y} | {z} | {int(not (x and y) or z)}')
